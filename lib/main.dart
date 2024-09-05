998cd5a503a2f5a4fece637677256dc11251dca2
james <pattanasinpluksek@gmail.com>
2024-09-05 09:36:31 +0700

    Ui_update

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
-import 'location.dart'; 
+import 'location.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Variable to hold the theme mode
+  ThemeMode _themeMode = ThemeMode.system;
+
+  // Function to toggle the theme mode
+  void _toggleTheme(bool isDarkMode) {
+    setState(() {
+      _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,15 +31,31 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home Page'),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.dark),
+        useMaterial3: true,
+      ),
+      themeMode: _themeMode, 
+      home: MyHomePage(
+        title: 'Home Page',
+        onThemeChanged: _toggleTheme, 
+        isDarkMode: _themeMode == ThemeMode.dark,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({
+    super.key,
+    required this.title,
+    required this.onThemeChanged,
+    required this.isDarkMode,
+  });
 
   final String title;
+  final Function(bool) onThemeChanged; 
+  final bool isDarkMode; 
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -32,7 +63,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   void _openMapScreen() {
-    // Navigate to MapScreen when the button is pressed
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => MapScreen()),
@@ -43,8 +73,15 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.secondary,
         title: Text(widget.title),
+        actions: [
+          Switch(
+            value: widget.isDarkMode,
+            onChanged: widget.onThemeChanged,
+            activeColor: Colors.white,
+          ),
+        ],
       ),
       body: Center(
         child: Column(
